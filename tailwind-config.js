745e88c6b6803d4f4fc20173c57d8b4bfc615175
jbraveheart <Justinebh23@gmail.com>
2026-04-15 10:04:16 -0500

    Add files via upload

---
+++ tailwind-config.js
@@ -0,0 +1,39 @@
+tailwind.config = {
+            darkMode: "class",
+            theme: {
+                extend: {
+                    "colors": {
+                        "surface": "#FCFAF8",
+                        "on-surface-variant": "#5C4D4D",
+                        "background": "#FCFAF8",
+                        "on-surface": "#2A2121",
+                        "surface-dim": "#EBE3E0",
+                        "surface-container": "#F4EBE8",
+                        "primary-container": "#8C001C",
+                        "on-primary": "#FFFFFF",
+                        "surface-container-lowest": "#FFFFFF",
+                        "surface-container-highest": "#E3D5D2",
+                        "surface-container-low": "#F9F3F1",
+                        "outline": "#C2B2B2",
+                        "primary": "#51000D",
+                        "outline-variant": "#E8DBDB",
+                        "secondary": "#C69214",
+                        "on-secondary": "#FFFFFF",
+                        "secondary-fixed": "#FFE18F",
+                        "secondary-fixed-dim": "#E5B02E",
+                        "secondary-container": "#FBE6AB",
+                        "on-secondary-container": "#4A3500",
+                        "surface-variant": "#E3D5D2",
+                        "on-secondary-fixed": "#2B1E00",
+                        "on-secondary-fixed-variant": "#4E3700",
+                        "inverse-surface": "#3D3131",
+                        "inverse-on-surface": "#FCFAF8"
+                    },
+                    "fontFamily": {
+                        "headline": ["Work Sans", "Montserrat", "sans-serif"],
+                        "body": ["Inter", "sans-serif"],
+                        "label": ["Inter", "sans-serif"]
+                    }
+                }
+            }
+        }
